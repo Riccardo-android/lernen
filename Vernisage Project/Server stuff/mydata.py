3c9008f72b634f027cc580140674547d71fe27a7
Friedfisch19 <friedrichreichardt@t-online.de>
2025-05-23 10:06:47 +0200

    added files to start working with mysql - WIP

---
+++ Vernisage Project/Server stuff/mydata.py
@@ -0,0 +1,25 @@
+from getpass import getpass
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host="localhost",
+        user=input("Enter username: "),
+        password=getpass("Enter password: "),
+    ) as connection:
+        create_db_query = "CREATE DATABASE IF NOT EXISTS mydb"
+        with connection.cursor() as cursor:
+         cursor.execute(create_db_query)
+
+        show_db_query = "SHOW DATABASES LIKE 'mydb'"
+        with connection.cursor() as cursor:
+            cursor.execute(show_db_query)
+            for db in cursor:
+                print(db)
+
+except Error as e:
+    print(e)
+
+
+
+

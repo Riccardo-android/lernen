e45f66f1627d2b86f35817aeef8a4d87b64294c8
Friedfisch19 <friedrichreichardt@t-online.de>
2025-05-31 21:15:51 +0200

    added 2 columns for database and added avg temp for 1961-1990

--- Vernisage-Project/data/sqldata.py
+++ Vernisage-Project/data/sqldata.py
@@ -3,15 +3,18 @@ import os
 import requests
 import mysql.connector
 import time
+
 from mysql.connector import errorcode
 from sympy.solvers.ode.single import SeparableReduced
+
+
 
 def getlines(filename, results):
    line = filename.split(sep="\r\n")
    results.extend(line)
    return(results)
 
-def getcells(results):
+def getcells(results, array2):
     for i in results:
         #i = i.replace(" ","")
         cells = i.split(sep=";")
@@ -19,7 +22,7 @@ def getcells(results):
         #array2 = [item.replace(" .", "0.") for item in cells]
     return array2
 
-def addZero(results):
+def addZero(results, array4):
     for i in range(len(results)):
         array3 = []
         for j in range(len(results[i])):
@@ -39,78 +42,92 @@ def create_database(cursor):
         print("Failed to create database: {}".format(err))
         exit(2)
 
-def filldatabase():
+def filldatabase(array4, array7, cnx, TABLE_NAME):
     array5 = array4
     del array5[0]
     del array5[len(array5) - 1]
+    del array7[0]
+    del array7[len(array7) - 1]
+    #print(array5)
+    for i in range(len(array5)):
+        for j in range(len(array7)):
+            if array5[i][0] == array7[j][0]:
+                array5[i].insert(1, array7[j][1])
+                array5[i].insert(2, array7[j][5])
+                break
+        else:
+            array5[i].insert(1, "unknown")
+            array5[i].insert(2, "unknown")
+
     id = [int(k[0]) for k in array5]
-    Bzeit = [str(k[1]) for k in array5]
-    source = [int(k[2]) for k in array5]
-    Jan = [float(k[3]) for k in array5]
-    Feb = [float(k[4]) for k in array5]
-    Mar = [float(k[5]) for k in array5]
-    Apr = [float(k[6]) for k in array5]
-    May = [float(k[7]) for k in array5]
-    Jun = [float(k[8]) for k in array5]
-    Jul = [float(k[9]) for k in array5]
-    Aug = [float(k[10]) for k in array5]
-    Sep = [float(k[11]) for k in array5]
-    Oct = [float(k[12]) for k in array5]
-    Nov = [float(k[13]) for k in array5]
-    Dec = [float(k[14]) for k in array5]
-    Jahr = [float(k[15]) for k in array5]
-    array6 = list(zip(id, Bzeit,source, Jan, Feb, Mar, Apr, May, Jun, Jul, Aug,Sep, Oct, Nov, Dec, Jahr))
+    ort = [str(k[1]) for k in array5]
+    bland = [str(k[2]) for k in array5]
+    bzzeit = [str(k[3]) for k in array5]
+    source = [int(k[4]) for k in array5]
+    Jan = [float(k[5]) for k in array5]
+    Feb = [float(k[6]) for k in array5]
+    Mar = [float(k[7]) for k in array5]
+    Apr = [float(k[8]) for k in array5]
+    May = [float(k[9]) for k in array5]
+    Jun = [float(k[10]) for k in array5]
+    Jul = [float(k[11]) for k in array5]
+    Aug = [float(k[12]) for k in array5]
+    Sep = [float(k[13]) for k in array5]
+    Oct = [float(k[14]) for k in array5]
+    Nov = [float(k[15]) for k in array5]
+    Dec = [float(k[16]) for k in array5]
+    Jahr = [float(k[17]) for k in array5]
+    array6 = list(zip(id, ort, bland, bzzeit,source, Jan, Feb, Mar, Apr, May, Jun, Jul, Aug,Sep, Oct, Nov, Dec, Jahr))
 
     with cnx.cursor() as cursor:
-        cursor.executemany('INSERT INTO  avg_temp_ger_9020 (Stations_id, Zeitraum, Datenquelle, Januar, Februar, Marz, April, Mai, Juni, Juli, August, September, Oktober, November, Dezember, avg_Jahr) '
-                    'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE Stations_id = Stations_id',array6)
+        cursor.executemany('INSERT INTO ' + TABLE_NAME + '  (Stations_id, Stationsname, Bundesland, Zeitraum, Datenquelle, Januar, Februar, Marz, April, Mai, Juni, Juli, August, September, Oktober, November, Dezember, avg_Jahr) '
+                    'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE Stations_id = Stations_id',array6)
 
 
-#connect with DWD API
-url = "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/multi_annual/mean_91-20/Temperatur_1991-2020.txt"
-response = requests.get(url)
-file = response.text
+def getdata(url, urlsname, DB_NAME, TABLE_NAME):
+    response = requests.get(url)
+    file = response.text
+    array = []
+    array2 = []
+    array4 = []
+    array7 = []
 
-array = []
-array2 = []
-array4 = []
+    getlines(file, array)
+    getcells(array, array2)
+    addZero(array2, array4)
+    array.clear()
+    array2.clear()
 
+    response = requests.get(urlsname)
+    file = response.text
+    getlines(file, array)
+    getcells(array, array2)
+    addZero(array2, array7)
 
-
-
-#query = "INSERT INTO table VALUES (%s)"
-#cur.execute(query, file_content,)
-
-
-
-getlines(file, array)
-getcells(array)
-addZero(array2)
-#print(array4)
-DB_NAME = 'hist_temp_ger'
-time.sleep(10)
-try:
-    cnx = mysql.connector.connect(user='root', password='Pr!m4bAl13rina', host="db", port=3306)
-    cursor = cnx.cursor()
     try:
-        cursor.execute("USE {}".format(DB_NAME))
+        cnx = mysql.connector.connect(user='root', password='Pr!m4bAl13rina', host="db", port=3306)
+        cursor = cnx.cursor()
+        try:
+            cursor.execute("USE {}".format(DB_NAME))
+        except mysql.connector.Error as err:
+            print("Failed to create database: {}".format(err))
+            if err.errno == errorcode.ER_BAD_DB_ERROR:
+                create_database(cursor)
+                print("Database {} created successfully".format(DB_NAME))
+                cnx.database = DB_NAME
+            else:
+                print(err)
+                exit(1)
     except mysql.connector.Error as err:
         print("Failed to create database: {}".format(err))
-        if err.errno == errorcode.ER_BAD_DB_ERROR:
-            create_database(cursor)
-            print("Database {} created successfully".format(DB_NAME))
-            cnx.database = DB_NAME
-        else:
-            print(err)
-            exit(1)
-except mysql.connector.Error as err:
-    print("Failed to create database: {}".format(err))
-    exit(3)
+        exit(3)
 
-TABLES = {}
-avgtemp = """CREATE TABLE IF NOT EXISTS avg_temp_ger_9020 (
-                                       `Stations_ID` int NOT NULL AUTO_INCREMENT, 
-                                       `Zeitraum` char(9) NOT NULL, 
+    TABLES = {}
+    avgtemp = """CREATE TABLE IF NOT EXISTS """ + TABLE_NAME + """ (
+                                       `Stations_ID` int NOT NULL AUTO_INCREMENT,
+                                       `Stationsname` varchar(255) NOT NULL,
+                                       `Bundesland` varchar(255) NOT NULL, 
+                                       `Zeitraum` varchar(255) NOT NULL, 
                                        `Datenquelle` int NOT NULL, 
                                        `Januar` float NOT NULL, 
                                        `Februar` float NOT NULL, 
@@ -125,23 +142,24 @@ avgtemp = """CREATE TABLE IF NOT EXISTS avg_temp_ger_9020 (
                                        `November` float NOT NULL, 
                                        `Dezember` float NOT NULL, 
                                        `avg_Jahr` float NOT NULL,
-                                       PRIMARY KEY (`Stations_ID`)
+                                       PRIMARY KEY (`Stations_ID`,`Stationsname`,`Zeitraum`)
                                        ) ENGINE=InnoDB
                                        """
 
-with cnx.cursor() as cursor:
-    cursor.execute(avgtemp)
-    cnx.commit()
-    filldatabase()
-    cnx.commit()
-    cnx.close()
+    with cnx.cursor() as cursor:
+        cursor.execute(avgtemp)
+        cnx.commit()
+        filldatabase(array4, array7, cnx, TABLE_NAME)
+        cnx.commit()
+        cnx.close()
 
 
 
-"""
-
-print(array2)
-#for x in array:
- #print(x)
-"""
-
+#connect with DWD API
+url = "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/multi_annual/mean_91-20/Temperatur_1991-2020.txt"
+urlsname = "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/multi_annual/mean_91-20/Temperatur_1991-2020_Stationsliste.txt"
+DB_NAME = "hist_climate_ger"
+TABLE_NAME = "avg_temp_ger_6120"
+getdata(url, urlsname, DB_NAME, TABLE_NAME)
+url = "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/multi_annual/mean_61-90/Temperatur_1961-1990.txt"
+getdata(url, urlsname, DB_NAME, TABLE_NAME)
